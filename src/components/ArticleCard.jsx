0928a05dd50387a89c75ae960a096e5a6aa323c2
GxrySu <garysu2013@gmail.com>
2022-11-01 10:59:05 +0000

    complete SingleCard page and refactor className

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 
 const ArticleCard = ({ article }) => {
-  console.log(article)
   const { article_id, author, created_at, title, topic } = article;
   const myTopic = topic.charAt(0).toUpperCase() + topic.slice(1);
   const myDate = created_at.substring(0, 10);
@@ -9,7 +8,7 @@ const ArticleCard = ({ article }) => {
   return (
     <section className="ArticleCard">
       <Link to={`/articles/${article_id}`}><h3>{title}</h3></Link>
-      <div className="Article">
+      <div className="Articles">
         <p>Topic: {myTopic}</p>
         <p>
           Author: <span>{author}</span>
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,5 +1,41 @@
-const SingleArticle = () => {
-    return <h2>Single Article</h2>
-}
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchArticleById } from "../fetch-api";
 
-export default SingleArticle
+const SingleArticle = () => {
+  const { article_id } = useParams();
+  const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchArticleById(article_id).then((articleData) => {
+      setArticle(articleData);
+      setIsLoading(false);
+    });
+  }, [article_id]);
+
+  const { author, body, comment_count, created_at, title, topic, votes } =
+    article;
+
+  return (
+    <div>
+      {isLoading ? (
+        <h2>Loading Article...</h2>
+      ) : (
+        <main className='Article'>
+          <h2>{title}</h2>
+          <br />
+          <p>Article ID: {article_id}</p>
+          <p>Author: {author}</p>
+          <p>Topic: {topic.charAt(0).toUpperCase() + topic.slice(1)}</p>
+          <p>{body}</p>
+          <p>Comments: {comment_count} </p>
+          <p>Date Created: {created_at.substring(0, 10)}</p>
+        </main>
+      )}
+    </div>
+  );
+};
+
+export default SingleArticle;
