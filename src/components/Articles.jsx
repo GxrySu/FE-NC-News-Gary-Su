1363b3ac1f5c3db9ff29eff98ec0547f1626dcbf
GxrySu <garysu2013@gmail.com>
2022-11-02 15:20:07 +0000

    add sortBy selector to Articles page

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,3 +1,4 @@
+import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 import { useState, useEffect } from "react";
 import { fetchArticles } from "../fetch-api";
 import ArticleCard from "./ArticleCard";
@@ -5,18 +6,51 @@ import ArticleCard from "./ArticleCard";
 const Articles = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortByInput, setSortByInput] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
+  const handleChange = (event) => {
+    setSortByInput(event.target.value);
+  };
+
+  const handleSortBySubmit = (event) => {
+    if (sortByInput) {
+      setSortBy(sortByInput);
+      setSortByInput("");
+    }
+    event.preventDefault();
+  };
 
   useEffect(() => {
     setIsLoading(true);
-    fetchArticles().then((articleList) => {
+    fetchArticles(sortBy).then((articleList) => {
       setArticles(articleList);
       setIsLoading(false);
     });
-  }, []);
+  }, [sortBy]);
 
   return (
     <div className="ArticlesList">
       <h2>List of Articles</h2>
+      <div className="Articles-SortBy">
+        <label>Sort By: </label>
+        <select
+          id="sortBy"
+          className="SortBy"
+          defaultValue=""
+          onChange={handleChange}
+        >
+          <option value="" disabled hidden>
+            ---Select Sort By---
+          </option>
+          <option value="title">Title</option>
+          <option value="topic">Topic</option>
+          <option value="author">Author</option>
+          <option value="created_at">Date</option>
+          <option value="votes">Votes</option>
+        </select>
+        <button onClick={handleSortBySubmit}>Sort</button>
+      </div>
       {isLoading ? (
         <h3>Loading Articles...</h3>
       ) : (
